a4dba3907265b34dafb60691db9e91166b1415a4
Puja Sharma <55825774+Puja-Sh@users.noreply.github.com>
2022-03-20 23:45:03 +0530

    Time: 180 ms (55.27%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-         int n = tops.size();
+        int n = tops.size();
         
-        // faceA for counting the occurence in tops
-		// faceB for counting occurence of numbers in bottoms
-        // same for counting when both tops and bottoms have same occurences
-        vector<int> faceA(7), faceB(7), same(7);
+        vector<int> face_top(7), face_bottom(7), face_same(7);
         
-        for(int i = 0; i < n; ++i)
-        {
-            ++faceA[tops[i]];
-            ++faceB[bottoms[i]];
+        for(int i=0; i<n; i++){
+            int top = tops[i];
+            int bottom = bottoms[i];
             
-            if(tops[i] == bottoms[i])
-                ++same[tops[i]];
+            ++face_top[top];
+            ++face_bottom[bottom];
+            
+            if(top == bottom) ++face_same[top];
         }
         
-        int minRotation = INT_MAX;
+        int min_rotation = INT_MAX;
         
-		// all possibilities from 1 to 6
-        for(int i = 1; i<=6; ++i)
-        {
-            if(faceA[i] + faceB[i] - same[i] == n)
-                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+        for(int i=1; i<=6; ++i){
+            if(face_top[i] + face_bottom[i] - face_same[i] == n){
+                
+                int min_face = min(face_top[i], face_bottom[i]) - face_same[i];
+                
+                min_rotation = min(min_rotation,min_face);
+            }
         }
         
-        return minRotation == INT_MAX ? -1 : minRotation;
-     
+        return min_rotation == INT_MAX ? -1 : min_rotation;
     }
 };
