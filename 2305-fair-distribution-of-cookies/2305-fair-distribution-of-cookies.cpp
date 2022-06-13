fdd6f955fff645ec22c5e38d17cd17964d6241ef
Puja Sharma <55825774+Puja-Sh@users.noreply.github.com>
2022-06-13 21:40:25 +0530

    Time: 1349 ms (50.00%), Space: 6.9 MB (50.00%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,42 +1,71 @@
+// class Solution {
+// public:
+//     // Backtracking 
+    
+//     int ans=INT_MAX;
+    
+//     void helper(vector<int>& cookies, vector<int>& kv, int idx){
+        
+//         if(idx >= cookies.size()){
+//             int temp = INT_MIN;
+//             for(int it:kv){
+//                 temp = max(temp, it);
+//             }
+            
+//             ans = min(ans, temp);
+            
+//             return;
+//         }
+        
+//         for(int i=0; i<kv.size(); i++){
+//             kv[i] += cookies[idx];      // add cookies at idx
+            
+//             helper(cookies, kv, idx+1); // call for next index cookies
+            
+//             kv[i] -= cookies[idx];  // on backtracking subtract the present index of cookies
+//         }
+        
+//         return;
+//     }
+    
+    
+    
+//     int distributeCookies(vector<int>& cookies, int k) {
+//         int n = cookies.size();
+       
+//         vector<int> kv(k, 0);
+        
+//         helper(cookies, kv, 0);
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    // Backtracking 
-    
-    int ans=INT_MAX;
-    
-    void helper(vector<int>& cookies, vector<int>& kv, int idx){
+    int ans = INT_MAX;
+    void solve(int idx, vector<int>& cookies, vector<int>& subset){
         
-        if(idx >= cookies.size()){
-            int temp = INT_MIN;
-            for(int it:kv){
-                temp = max(temp, it);
+        if(idx==cookies.size()){
+            int maxm = INT_MIN;
+            for(int i=0;i<subset.size();i++){
+                maxm = max(maxm,subset[i]);
             }
-            
-            ans = min(ans, temp);
-            
+            ans = min(ans,maxm);
             return;
         }
         
-        for(int i=0; i<kv.size(); i++){
-            kv[i] += cookies[idx];
-            
-            helper(cookies, kv, idx+1);
-            
-            kv[i] -= cookies[idx];
+        for(int i=0;i<subset.size();i++){
+            subset[i] += cookies[idx];
+            solve(idx+1, cookies, subset);
+            subset[i]-= cookies[idx];
         }
-        
-        return;
     }
-    
-    
     
     int distributeCookies(vector<int>& cookies, int k) {
         int n = cookies.size();
-       
-        vector<int> kv(k, 0);
-        
-        helper(cookies, kv, 0);
-        
+        vector<int> subset(k,0); 
+        solve(0,cookies, subset);
         return ans;
     }
 };
